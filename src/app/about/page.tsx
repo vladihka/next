b8c274a3df96ec33381d76361fe0e50921df9d36
vladihka <vladihka58@gmail.com>
2025-10-20 11:55:02 +0200

    Added new pages (Contact, About) and metadata for each page.
    Added Contact and About pages with basic content.
    Added metadata (title and description) for SEO.

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: "About",
+  description: "Learn more About Us",
+};
+
+export default function About() {
+  return (
+    <div>About Us</div>
+  );
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: "Contact",
+  description: "Get in touch with Us",
+};
+
+export default function Contact() {
+  return (
+    <div>Contact Us</div>
+  );
+}
